27914e71a3eec27569eaeeb3cd60c38ec40c307d
Luis6omez7rujillo <luisgomeztrujillo@gmail.com>
2021-11-26 18:41:36 -0500

    crud entidad migrante

--- Aplicacion/Presentacion/Pages/Migrantes/Detalles.cshtml.cs
+++ Aplicacion/Presentacion/Pages/Migrantes/Detalles.cshtml.cs
@@ -5,12 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Dominio;
+using Persistencia;
+
 namespace Presentacion.Pages.Migrantes
 {
     public class DetallesModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMigrante _repomigrante;
+        public DetallesModel(IRepositorioMigrante repomigrante)
         {
+            this._repomigrante=repomigrante;
+        }
+        [BindProperty]
+        public Migrante Migrante{get;set;}
+
+        public ActionResult OnGet(int id)
+        {
+            Migrante= _repomigrante.BuscarMigrante(id);
+            if (Migrante==null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
