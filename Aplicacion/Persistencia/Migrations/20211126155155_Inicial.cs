1438a830cf50f7c911c6544d78f06d3692851e9e
Luis6omez7rujillo <luisgomeztrujillo@gmail.com>
2021-11-26 14:47:36 -0500

    Creada Entidad Migrante con Repositorio CRUD y Capa Presentacion Basica-Grupo 36

---
+++ Aplicacion/Persistencia/Migrations/20211126155155_Inicial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medidas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Estatura = table.Column<float>(type: "real", nullable: false),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    Imc = table.Column<float>(type: "real", nullable: false),
+                    IdPersonaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medidas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medidas_Personas_IdPersonaId",
+                        column: x => x.IdPersonaId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medidas_IdPersonaId",
+                table: "Medidas",
+                column: "IdPersonaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medidas");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
